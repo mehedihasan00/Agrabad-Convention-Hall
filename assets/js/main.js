75a7b4e454092c0cbc90c3b1ad389cda1c14e11c
Mehedi Hasan <67319547+mehedihasan00@users.noreply.github.com>
2022-05-21 18:40:24 +0600

    21/05/22

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@
   
     // Sticky Nav
       $(window).on('scroll', function() {
-          if ($(window).scrollTop() > 200) {
+          if ($(window).scrollTop() > 150) {
               $('.scrolling-navbar').addClass('top-nav-collapse');
           } else {
               $('.scrolling-navbar').removeClass('top-nav-collapse');
